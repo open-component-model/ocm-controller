d797203f13fca3ff748d047f189542363f06d03a
Piaras Hoban <phoban01@gmail.com>
2022-09-14 12:48:02 +0100

    implement oci registry server setup on controller launch, with flags controlling the port and storage directory
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ import (
 
 	ocmcontrollerv1 "github.com/open-component-model/ocm-controller/api/v1alpha1"
 	"github.com/open-component-model/ocm-controller/controllers"
+	"github.com/open-component-model/ocm-controller/pkg/registry"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -52,8 +53,13 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var ociRegistryAddr string
+	var ociRegistryStorageDir string
+
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&ociRegistryAddr, "oci-registry-bind-address", ":5000", "The address the oci registry binds to.")
+	flag.StringVar(&ociRegistryStorageDir, "oci-registry-storage-dir", "/tmp", "The location on disk that will contain the oci registry content.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -127,6 +133,32 @@ func main() {
 		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
+
+	c := registry.Config{
+		StorageDirectory: ociRegistryStorageDir,
+		Addr:             ociRegistryAddr,
+	}
+
+	r, err := registry.NewRegistry(c)
+	if err != nil {
+		setupLog.Error(err, "failed to create oci registry")
+		os.Exit(1)
+	}
+
+	// start the registry
+	go func() {
+		// Block until our controller manager is elected leader. We presume our
+		// entire process will terminate if we lose leadership, so we don't need
+		// to handle that.
+		<-mgr.Elected()
+
+		setupLog.Info("starting oci registry server")
+
+		if err := r.ListenAndServe(); err != nil {
+			setupLog.Error(err, "failed to start oci registry server")
+			os.Exit(1)
+		}
+	}()
 
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
