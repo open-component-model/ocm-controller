116a3c9759137afbce2bb8bfc1b011feb9805415
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-09-20 14:46:10 +0200

    Set up the basics of the controller for workflow classes.

--- controllers/workflow_controller.go
+++ controllers/workflow_controller.go
@@ -19,23 +19,18 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"time"
 
 	actionv1 "github.com/open-component-model/ocm-controller/api/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/cluster-api/controllers/external"
-	"sigs.k8s.io/cluster-api/util/patch"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-// RealizationReconciler reconciles a Realization object.
-type RealizationReconciler struct {
+// WorkflowClassReconciler reconciles a Realization object.
+type WorkflowClassReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 
@@ -51,12 +46,12 @@ type RealizationReconciler struct {
 // move the current state of the cluster closer to the desired state.
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
-func (r *RealizationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := log.FromContext(ctx).WithName("action-controller")
+func (r *WorkflowClassReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx).WithName("workflow-class-controller")
 	log.V(4).Info("starting reconcile loop")
 
-	action := &actionv1.Action{}
-	if err := r.Client.Get(ctx, req.NamespacedName, action); err != nil {
+	workflowClass := &actionv1.WorkflowClass{}
+	if err := r.Client.Get(ctx, req.NamespacedName, workflowClass); err != nil {
 		if apierrors.IsNotFound(err) {
 			// Object not found, return.  Created objects are automatically garbage collected.
 			// For additional cleanup logic use finalizers.
@@ -66,53 +61,15 @@ func (r *RealizationReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		// Error reading the object - requeue the request.
 		return ctrl.Result{}, fmt.Errorf("failed to get action object: %w", err)
 	}
-	log.V(4).Info("reconciling action", "action", action)
-	providerObj, err := Get(ctx, r.Client, &corev1.ObjectReference{
-		Kind:       action.Spec.ProviderRef.Kind,
-		Name:       action.Spec.ProviderRef.Name,
-		APIVersion: action.Spec.ProviderRef.ApiVersion,
-	}, action.Namespace)
-	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to find owner for action: %w", err)
-	}
-	log.V(4).Info("found provider object", "provider", providerObj)
-	// Set external object ControllerReference to the provider ref.
-	if err := controllerutil.SetControllerReference(action, providerObj, r.Client.Scheme()); err != nil {
-		return ctrl.Result{
-			RequeueAfter: 1 * time.Minute,
-		}, fmt.Errorf("failed to set owner reference: %w", err)
-	}
-
-	log.V(4).Info("set up controller reference")
-	// Initialize the patch helper.
-	patchHelper, err := patch.NewHelper(providerObj, r.Client)
-	if err != nil {
-		return ctrl.Result{
-			RequeueAfter: 1 * time.Minute,
-		}, fmt.Errorf("failed to create patch helper: %w", err)
-	}
-
-	// Patch the external object.
-	if err := patchHelper.Patch(ctx, providerObj); err != nil {
-		return ctrl.Result{
-			RequeueAfter: 1 * time.Minute,
-		}, fmt.Errorf("failed to patch provider object: %w", err)
-	}
-	log.V(4).Info("path successful")
-	// Ensure we add a watcher to the external object.
-	if err := r.externalTracker.Watch(ctrl.Log, providerObj, &handler.EnqueueRequestForOwner{OwnerType: &actionv1.Action{}}); err != nil {
-		return ctrl.Result{
-			RequeueAfter: 1 * time.Minute,
-		}, fmt.Errorf("failed to set up watch for parent object: %w", err)
-	}
+	log.V(4).Info("reconciling workflow class", "workflow-class", workflowClass)
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *RealizationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *WorkflowClassReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	controller, err := ctrl.NewControllerManagedBy(mgr).
-		For(&actionv1.Action{}).
+		For(&actionv1.WorkflowClass{}).
 		Build(r)
 
 	if err != nil {
