2fcac1313a98bec1daedced2b77254a90b9e0f46
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-11-11 14:19:54 +0100

    Updated the default port for OCI registry

--- main.go
+++ main.go
@@ -60,9 +60,8 @@ func main() {
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.StringVar(&ociRegistryPort, "oci-registry-port", "5001", "The port the oci registry binds to.")
-	// flag.StringVar(&ociRegistryServicceName, "oci-registry-service-name", "registry.ocm-system.svc.cluster.local",
-	flag.StringVar(&ociRegistryServicceName, "oci-registry-service-name", "localhost:5001",
+	flag.StringVar(&ociRegistryPort, "oci-registry-port", "5000", "The port the oci registry binds to.")
+	flag.StringVar(&ociRegistryServicceName, "oci-registry-service-name", "localhost:5000",
 		"Service")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
