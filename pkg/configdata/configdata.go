1834a84cf3b30a657666e2edfb9bc6af88db5c38
Piaras Hoban <4415593+phoban01@users.noreply.github.com>
2023-01-24 09:29:15 +0000

    add support for custom mappings via cue (#88)
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>
    
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- pkg/configdata/configdata.go
+++ pkg/configdata/configdata.go
@@ -32,9 +32,15 @@ type LocalizationRule struct {
 	Resource   ResourceItem `json:"resource"`
 	File       string       `json:"file"`
 	Registry   string       `json:"registry,omitempty"`
+	Mapping    *Mapping     `json:"mapping,omitempty"`
 	Repository string       `json:"repository,omitempty"`
 	Image      string       `json:"image,omitempty"`
 	Tag        string       `json:"tag,omitempty"`
+}
+
+type Mapping struct {
+	Path      string `json:"path"`
+	Transform string `json:"transform"`
 }
 
 type ResourceItem struct {
