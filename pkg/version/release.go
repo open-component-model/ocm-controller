2b66c97aab4d12e8082c0f71f7c0c53d33cbc270
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-03-01 13:57:28 +0100

    added release notes for 0.5.0 (#143)

--- pkg/version/release.go
+++ pkg/version/release.go
@@ -5,7 +5,7 @@
 package version
 
 // ReleaseVersion is the version number in semver format "vX.Y.Z", prefixed with "v".
-var ReleaseVersion = "v0.4.0"
+var ReleaseVersion = "v0.5.0"
 
 // ReleaseCandidate is the release candidate ID in format "rc.X", which will be appended to the release version.
 var ReleaseCandidate = "rc.1"
