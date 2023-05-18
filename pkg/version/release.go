5539723ed618642ef3af4ee6f60a1ee5b57b9ef2
Piaras Hoban <phoban01@gmail.com>
2023-05-18 10:09:26 +0100

    add release notes and bump verison to v0.8.0
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- pkg/version/release.go
+++ pkg/version/release.go
@@ -5,7 +5,7 @@
 package version
 
 // ReleaseVersion is the version number in semver format "vX.Y.Z", prefixed with "v".
-var ReleaseVersion = "v0.7.1"
+var ReleaseVersion = "v0.8.0"
 
 // ReleaseCandidate is the release candidate ID in format "rc.X", which will be appended to the release version.
 var ReleaseCandidate = "rc.1"
