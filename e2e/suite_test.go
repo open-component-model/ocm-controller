5d2cc3be67a46bc44bed61c382787079d0db7ec5
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-06-29 09:44:23 +0200

    Adding some log dumpings

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -7,7 +7,11 @@
 package e2e
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/open-component-model/ocm-e2e-framework/shared"
@@ -39,10 +43,6 @@ func TestMain(m *testing.M) {
 	stopChannelRegistry := make(chan struct{}, 1)
 	stopChannelGitea := make(chan struct{}, 1)
 
-	//if err := os.Setenv("REGISTRY_DISABLE_HTTPS", "1"); err != nil {
-	//	os.Exit(1)
-	//}
-
 	testEnv.Setup(
 		envfuncs.CreateKindCluster(kindClusterName),
 		envfuncs.CreateNamespace(namespace),
@@ -54,6 +54,7 @@ func TestMain(m *testing.M) {
 	)
 
 	testEnv.Finish(
+		dumpControllerLogs(namespace),
 		shared.RemoveGitServer(namespace),
 		shared.ShutdownPortForward(stopChannelRegistry),
 		shared.ShutdownPortForward(stopChannelGitea),
@@ -62,4 +63,31 @@ func TestMain(m *testing.M) {
 	)
 
 	os.Exit(testEnv.Run(m))
+}
+
+func dumpControllerLogs(n string) env.Func {
+	return func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+		fmt.Println("dumping logs")
+		// kubectl logs `k get pods --template '{{range .items}}{{.metadata.name}}{{end}}' --selector=app=ocm-controller -n ocm-system` -n ocm-system -f
+		cmd := exec.CommandContext(ctx, "kubectl", "get", "pods", "--template", "'{{range .items}}{{.metadata.name}}{{end}}'", "--selector=app=ocm-controller", "-n", "ocm-system")
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("ERROR 1: ", err, string(output))
+			return ctx, fmt.Errorf("failed to get pods: %w", err)
+		}
+
+		pod := strings.ReplaceAll(string(output), "'", "")
+		fmt.Println("getting logs for pod: ", pod)
+
+		cmd = exec.CommandContext(ctx, "kubectl", "logs", pod, "-n", "ocm-system")
+		output, err = cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("ERROR 2: ", err, string(output))
+			return ctx, fmt.Errorf("failed to gather logs from pod %s: %w", string(output), err)
+		}
+
+		fmt.Println(string(output))
+
+		return ctx, nil
+	}
 }
