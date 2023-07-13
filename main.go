9990d6a81b22ac9b36e9fa807c79113654ac8678
Piaras Hoban <phoban01@gmail.com>
2023-07-13 16:11:45 +0100

    setup the CRD for ResourcePipeline
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- main.go
+++ main.go
@@ -200,6 +200,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "FluxDeployer")
 		os.Exit(1)
 	}
+	if err = (&controllers.ResourcePipelineReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ResourcePipeline")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
