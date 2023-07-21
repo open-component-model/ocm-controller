d406634815707a4449ea239e41c72fe8bf80d16c
Piaras Hoban <phoban01@gmail.com>
2023-07-21 15:09:06 +0100

    [wip] add tinygo build tag to convert, logging and ocm packages
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- pkg/wasm/convert/strings.go
+++ pkg/wasm/convert/strings.go
@@ -1,6 +1,16 @@
+//go:build tinygo.wasm
+
 package convert
 
-import "unsafe"
+// This file is designed to be imported by plugins.
+
+// #include <stdlib.h>
+import "C"
+
+import (
+	"reflect"
+	"unsafe"
+)
 
 // PointerToString returns a string from WebAssembly compatible numeric types
 // representing its pointer and length.
@@ -9,7 +19,7 @@ import "unsafe"
 func PointerToString(v uint64) string {
 	ptr := uint32(v >> 32)
 	size := uint32(v & 0xffffffff)
-	return unsafe.String((*byte)(unsafe.Pointer(uintptr(ptr))), size)
+	return string(ptrToByte(ptr, size))
 }
 
 // StringToPointer returns a pointer and size pair for the given string in a way
@@ -17,6 +27,31 @@ func PointerToString(v uint64) string {
 // The returned pointer aliases the string hence the string must be kept alive
 // until ptr is no longer needed.
 func StringToPointer(s string) (uint32, uint32) {
-	ptr := unsafe.Pointer(unsafe.StringData(s))
-	return uint32(uintptr(ptr)), uint32(len(s))
+	return byteToPtr([]byte(s))
+}
+
+func ptrToByte(ptr, size uint32) []byte {
+	var b []byte
+	s := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	s.Len = uintptr(size)
+	s.Cap = uintptr(size)
+	s.Data = uintptr(ptr)
+	return b
+}
+
+func byteToPtr(buf []byte) (uint32, uint32) {
+	if len(buf) == 0 {
+		return 0, 0
+	}
+
+	size := C.ulong(len(buf))
+	ptr := unsafe.Pointer(C.malloc(size))
+
+	copy(unsafe.Slice((*byte)(ptr), size), buf)
+
+	return uint32(uintptr(ptr)), uint32(len(buf))
+}
+
+func FreePtr(ptr uint32) {
+	C.free(unsafe.Pointer(uintptr(ptr)))
 }
