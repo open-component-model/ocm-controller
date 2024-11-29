be8a6b5ad9f185fd43d531f8ea857d372e3994e6
Frederic Wilhelm <frederic.wilhelm02@sap.com>
2024-11-29 17:09:53 +0100

    cleanup; refactoring

--- hack/prime_test_cluster.sh
+++ hack/prime_test_cluster.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # cleanup
-rm -fr hack/rootCA.pem
+if [[ -f hack/rootCA.pem ]]; then
+  rm hack/rootCA.pem
+fi
 
 CERT_MANAGER_VERSION=${CERT_MANAGER_VERSION:-v1.13.1}
 
@@ -12,23 +14,23 @@ fi
 
 kind create cluster --name=e2e-test-cluster
 
-echo -n 'installing cert-manager'
+echo 'installing cert-manager'
 kubectl apply -f hack/cert-manager.yaml
 kubectl wait --for=condition=Available=True Deployment/cert-manager -n cert-manager --timeout=60s
 kubectl wait --for=condition=Available=True Deployment/cert-manager-webhook -n cert-manager --timeout=60s
 kubectl wait --for=condition=Available=True Deployment/cert-manager-cainjector -n cert-manager --timeout=60s
 echo 'done'
 
-echo -n 'applying root certificate issuer'
+echo 'applying root certificate issuer'
 kubectl apply -f hack/cluster_issuer.yaml
 echo 'done'
 
-echo -n 'waiting for root certificate to be generated...'
+echo 'waiting for root certificate to be generated...'
 kubectl wait --for=condition=Ready=true Certificate/mpas-bootstrap-certificate -n cert-manager --timeout=60s
 echo 'done'
 
 kubectl get secret ocm-registry-tls-certs -n cert-manager -o jsonpath="{.data['tls\.crt']}" | base64 -d > hack/rootCA.pem
-echo -n 'installing root certificate into local trust store...'
+echo 'installing root certificate into local trust store...'
 CAROOT=hack ./bin/mkcert -install
 rootCAPath="./hack/rootCA.pem"
 
