81196f8af381390fc8d0506d6e437e4c5e4fe9bf
Gerald Morrison <gerald.gm.morrison@gmail.com>
2025-02-19 12:52:00 +0100

    get gosec working

--- api/v1alpha1/constants.go
+++ api/v1alpha1/constants.go
@@ -2,7 +2,7 @@ package v1alpha1
 
 const (
 	// DefaultRegistryCertificateSecretName is the name of the of certificate secret for client and registry.
-	DefaultRegistryCertificateSecretName = "ocm-registry-tls-certs" //nolint:gosec // not a credential
+	DefaultRegistryCertificateSecretName = "ocm-registry-tls-certs" // nolint:gosec // not a credential
 )
 
 // Internal ExtraIdentity keys.
