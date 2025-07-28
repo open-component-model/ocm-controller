aa748ab6555efbf3a0f0f001ad201d15025a92f7
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2025-07-28 09:44:29 +0200

    chore: release docs for v0.26.6
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

--- pkg/version/release.go
+++ pkg/version/release.go
@@ -1,7 +1,7 @@
 package version
 
 // ReleaseVersion is the version number in semver format "vX.Y.Z", prefixed with "v".
-var ReleaseVersion = "v0.26.5"
+var ReleaseVersion = "v0.26.6"
 
 // ReleaseCandidate is the release candidate ID in format "rc.X", which will be appended to the release version.
 var ReleaseCandidate = "rc.1"
