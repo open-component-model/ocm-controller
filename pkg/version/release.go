d5588cbbd165590abb00590ee5f2a97d2ac5fc77
Gergely Brautigam <gergely.brautigam@sap.com>
2025-08-10 18:55:20 +0200

    chore: release docs for v0.26.7
    Signed-off-by: Gergely Brautigam <gergely.brautigam@sap.com>

--- pkg/version/release.go
+++ pkg/version/release.go
@@ -1,7 +1,7 @@
 package version
 
 // ReleaseVersion is the version number in semver format "vX.Y.Z", prefixed with "v".
-var ReleaseVersion = "v0.26.6"
+var ReleaseVersion = "v0.26.7"
 
 // ReleaseCandidate is the release candidate ID in format "rc.X", which will be appended to the release version.
 var ReleaseCandidate = "rc.1"
